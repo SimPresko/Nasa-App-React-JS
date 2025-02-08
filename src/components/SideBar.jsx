dac5b7a087d670913a59c12666a0b50e73931da0
SimPresko <preslibankov@gmail.com>
2025-02-08 20:59:36 +0100

    Fetching data from the Nasa API

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
 export default function SideBar(props) {
-  const{handleToggleModal} = props
+  const{handleToggleModal, data} = props
   return (
     <div className='sidebar'>
       <div className='bgOverlay' onClick={handleToggleModal}></div>
       <div className='sidebarContents'>
-        <h2>The Brutal Martian Landscape</h2>
-        <div>
-          <p>Description</p>
-          <p>f2knve2lvbn2elv wrmw mkmkewqme wqeqwe wqe qwmkmwqkmfmqw klmklqwm 
-            qwfqwfqwfqwfqwfwq
-          </p>
+        <h2>{data?.title}</h2>
+        <div className='descriptionContainer'>
+          <p className='descriptionTitle'>{data?.date}</p>
+          <p>{data?.explanation}</p>
         </div>
         <button onClick={handleToggleModal}>
           <i className="fa-solid fa-arrow-right"></i>
